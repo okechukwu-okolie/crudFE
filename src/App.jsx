8897ac2d48caa1391781684ce7fe6a55bad5a7d8
Okolie Samuel <okechukwu.okolie.2023@gmail.com>
2025-07-28 08:15:40 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 class="text-3xl mx-auto w-[100%] text-yellow-500 font-bold underline">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
