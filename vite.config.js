369ec7e4ffd8956ad3e4649cd102b2ffc42bf0c1
Okolie Samuel <okechukwu.okolie.2023@gmail.com>
2025-07-28 14:10:53 +0100

    another commit

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
     react(),
   tailwindcss(),
 ],
+// server:{
+//   proxy:{
+//     '/books':{
+//       target:'http://localhost:3000',
+//       changeOrigin:true,
+//     }
+//   }
+// }
 })
