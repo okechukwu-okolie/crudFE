c22aab2dc2c45d34fb5790c580e5e0e04249dd9e
Okolie Samuel <okechukwu.okolie.2023@gmail.com>
2025-07-28 10:00:44 +0100

    adding to frontend code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBooks from './pages/CreateBooks'
+import ShowBook from './pages/ShowBook'
+import EditBook from './pages/EditBook'
+import DeleteBook from './pages/DeleteBook'
 
 function App() {
   
 
   return (
-    <>
-    <h1 class="text-3xl mx-auto w-[100%] text-yellow-500 font-bold underline">
-    Hello world!
-  </h1>
-    </>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/books/create' element={<CreateBooks/>} />
+      <Route path='/books/details/:id' element={<ShowBook/>} />
+      <Route path='/books/edit/:id' element={<EditBook/>} />
+      <Route path='/books/delete/:id' element={<DeleteBook/>} />
+    </Routes>
   )
 }
 
---
+++ src/pages/ShowBook.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const ShowBook = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShowBook
